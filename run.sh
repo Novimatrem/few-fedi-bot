3e64dda46dba48cc7c8066f9717537e06ccd89d9
Novimatrem <69379708+Novimatrem@users.noreply.github.com>
2025-08-02 23:53:34 +0100

    2025 version

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-
+clear
 echo "waiting for server to boot"
+echo "don't touch anything"
+#echo "CLICK ON THE TEXT ENTRY BOX NOW!"
 sleep 15s
 
 # you need to specifically have firefox open, and switched to
-# the mastodon homepage/dashboard/main thing, in a position ready to toot.
+
 # it should be the only tab, and you should be switched to it
 
 # sudo apt install -y xclip
@@ -44,9 +46,9 @@ sleep 2s
 sleep 1s
 wmctrl -a "Mozilla Firefox"
 sleep 1s
-xdotool getactivewindow key shift+Tab
+#xdotool getactivewindow key Tab
 sleep 1s
-xdotool key 1
+#xdotool key 1
 sleep 1s
 xdotool getactivewindow keyup shift
 xdotool getactivewindow keyup ctrl
@@ -55,10 +57,18 @@ xdotool keyup n
 xdotool keyup shift
 xdotool keyup Tab
 xdotool keyup ctrl
+xdotool key F5
 sleep 1s
-xdotool keydown n
+#xdotool keydown n
 sleep 0.2s
 xdotool keyup n
+sleep 1s
+xdotool key Tab
+echo "we in box now"
+sleep 1s
+xdotool getactivewindow key ctrl+a
+sleep 1s
+xdotool key BackSpace
 sleep 1s
 xdotool getactivewindow key ctrl+v
 sleep 1s
@@ -66,9 +76,79 @@ xdotool keyup Ctrl
 xdotool keyup Return
 sleep 1s
 xdotool key Ctrl+Return
+sleep 1s
+#xdotool key Tab
+sleep 1s
+xdotool key Tab
+sleep 1s
+xdotool key Tab
+sleep 1s
+xdotool key Tab
+sleep 1s
+xdotool key Return
+sleep 1s
 
+# resize the window so it is correct. hack
+xdotool key Super+Right
+sleep 1s
+xdotool key Super+Up
+sleep 1s
+xdotool key Super+Right
+sleep 1s
+xdotool key Super+Left
+sleep 1s
+xdotool key Super+Left
+sleep 1s
+xdotool key Super+Left
+sleep 1s
+
+# NOW WE MANUALLY CLICK THE PUBLIC BUTTON WITH X,Y COORDS BECAUSE FUCK A11Y IG
+# WHY DID THEY DO THIS
+sleep 1s
+xdotool mousemove 500 440
+
+sleep 1s
+
+xdotool click 1
+
+
+xdotool key Shift+Tab
+sleep 1s
+xdotool key Shift+Tab
+
+echo "send key"
+xdotool key Return # send key
+echo ""
+echo "WE JUST POSTED, NOW WE MUST RETURN TO THE POSTING PAGE."
+echo ""
+sleep 1s
+xdotool key Tab
+sleep 1s
+xdotool key Tab
+sleep 1s
+xdotool key Tab
+sleep 1s
+xdotool key Tab
+sleep 1s
+xdotool key Return
+sleep 1s
+
+echo ""
+echo "reached end of loop, waiting 1 hour"
+echo ""
+
+# sleep 1 min for testing
+sleep 60s
 
 sleep 3600s
 # 1 hour in seconds
+sleep 3600s
+# 1 hour in seconds
+sleep 3600s
+# 1 hour in seconds
+sleep 3600s
+# 1 hour in seconds
+
 
 bash run.sh
+
